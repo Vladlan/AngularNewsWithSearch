773043fb9aa3b75ca29205301928b00e63ae15e8
Vladlan <lanvlad911@gmail.com>
2018-04-01 19:07:05 +0300

    finished Forms mod #7 - Dynamicly changing forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
 
 
@@ -10,12 +10,18 @@ import {NgForm} from '@angular/forms';
   styles: [
     `input.ng-invalid.ng-touched { border: solid red 1px }`,
     `.error { color: red }`,
-    `.container {
-      margin-top: 3%;
+    `.container {margin-top: 3%;}`,
+    `.code {
+      background-color: #f2f2f2;
+      border: 1px solid silver;
+      margin-top: 10px;
+      border-radius: 5px;
     }`
   ]
 })
 export class AppComponent implements OnInit {
+  @ViewChild('form') form: NgForm;
+
   answers = [{
     type: 'yes',
     text: 'да'
@@ -29,9 +35,30 @@ export class AppComponent implements OnInit {
   defaultAnswer = 'yes';
   defaultCountry = 'by';
 
+  formData = {};
+  isSubmited = false;
+
+  addRandEmail () {
+    const randEmail = 'randEmail@mail.com';
+    // this.form.setValue({
+    //   user: {
+    //     password: '',
+    //     email: 'randEmail'
+    //   },
+    //   country: '',
+    //   answer: ''
+    // });
+    this.form.form.patchValue({
+      user: {email: randEmail}
+    });
+  }
+
   submitForm(form: NgForm) {
     console.log('Submitted');
-    console.log(form);
+    console.log(this.form);
+    this.isSubmited=true;
+    this.formData = this.form.value;
+    this.form.reset();
   }
 
   constructor() {
