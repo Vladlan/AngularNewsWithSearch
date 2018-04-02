968356262cc38e009c7e9c824205d6749d69f491
Vladlan <lanvlad911@gmail.com>
2018-04-02 20:50:31 +0300

    finished HTTP mod #1 - Server setup (json-server)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, NgForm, Validators} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 
 @Component({
@@ -22,57 +22,19 @@ import {FormControl, FormGroup, NgForm, Validators} from '@angular/forms';
     }`
   ]
 })
+
 export class AppComponent implements OnInit {
-  answers = [{
-    type: 'yes',
-    text: 'да'
-  },
+
+  cars = [
     {
-      type: 'no',
-      text: 'нет'
+      name: 'Ford',
+      color: 'white',
+      id: 1
     }
   ];
 
-  charsCount = 5;
+  constructor() {}
 
-  isSubmited = false;
+  ngOnInit() {}
 
-  form: FormGroup;
-
-  onSubmit() {
-    console.log('Submitted');
-    console.log(this.form);
-    this.isSubmited = true;
-  }
-
-  constructor() {
-  }
-
-  ngOnInit() {
-    this.form = new FormGroup({
-      user: new FormGroup({
-        email: new FormControl('defaultValue', [Validators.required, Validators.email], this.checkForEmail),
-        password: new FormControl('', [Validators.required, this.checkForLength.bind(this)])
-      }),
-      country: new FormControl('ru'),
-      answer: new FormControl('no')
-    });
-  }
-
-  checkForLength(control: FormControl) {
-    if (control.value.length <= this.charsCount) {
-      return {'lengthError': true};
-    }
-    return null;
-  }
-
-  checkForEmail(control: FormControl): Promise<any> {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (control.value === 'defaultValue@smt') {
-          resolve( {'emailIsUsed': true} );
-        } else { resolve(null); }
-      }, 3000);
-    });
-  }
 }
