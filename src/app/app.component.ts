b7de4b50404da99c375107647409dd6c64e6c077
Vladlan <lanvlad911@gmail.com>
2018-04-02 21:13:53 +0300

    finished HTTP mod #2 - Fetching data with Method GET

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,27 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {CarsService} from './cars.service';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [],
-  styles: [
-    `input.ng-invalid.ng-touched { border: solid red 1px }`,
-    `.error { color: red }`,
-    `.container {margin-top: 3%;}`,
-    `.code {
-      background-color: #f2f2f2;
-      border: 1px solid silver;
-      margin-top: 10px;
-      border-radius: 5px;
-    }`,
-      `label {
-      font-weight: 800;
-    }`
-  ]
+  providers: []
 })
 
 export class AppComponent implements OnInit {
 
-  cars = [
-    {
-      name: 'Ford',
-      color: 'white',
-      id: 1
-    }
-  ];
+  cars = [];
 
-  constructor() {}
+  constructor(private carsService: CarsService) {}
 
   ngOnInit() {}
 
+  loadCars() {
+    this.carsService
+      .getCars()
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
 }
