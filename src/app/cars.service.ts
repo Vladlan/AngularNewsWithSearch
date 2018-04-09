dadd53e481aa9e1787f34a3418d233e7abbba493
Vladlan <lanvlad911@gmail.com>
2018-04-09 20:33:01 +0300

    finished Routes mod #6 - Parameters of adress string PART #2

--- src/app/cars.service.ts
+++ src/app/cars.service.ts
@@ -5,15 +5,21 @@ export class CarsService {
   cars = [
     {
       name: 'Ford',
-      id: 1
+      id: 1,
+      year: 2011,
+      color: 'black'
     },
     {
       name: 'Audi',
-      id: 2
+      id: 2,
+      year: 2013,
+      color: 'red'
     },
     {
       name: 'Bentley',
-      id: 3
+      id: 3,
+      year: 2012,
+      color: 'green'
     }
   ];
 }
